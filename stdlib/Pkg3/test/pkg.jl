b2255507a180b5af8681644302a2aee9db89ba81
Kristoffer Carlsson <kcarlsson89@gmail.com>
2018-03-07 10:19:49 +0100

    try this

--- stdlib/Pkg3/test/pkg.jl
+++ stdlib/Pkg3/test/pkg.jl
@@ -15,8 +15,8 @@ include("utils.jl")
 
 const TEST_PKG = (name = "Example", uuid = UUID("7876af07-990d-54b4-ab0e-23690620f79a"))
 
-@show Pkg3.Types.STDLIB_DIR
-@show isdir(Pkg3.Types.STDLIB_DIR)
+@show Pkg3.Types.stdlib_dir()
+@show isdir(Pkg3.Types.stdlib_dir())
 @show @__DIR__
 
 temp_pkg_dir() do project_path
